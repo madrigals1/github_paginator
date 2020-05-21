d4838409b4d51dae121d4523be2b779d758bd501
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-05-21 15:35:16 +0700

    [UPD] Github token is now in Docs

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,17 @@
+/**
+ * Module with constants
+ * @module Constants
+ */
+
 const dotenv = require('dotenv');
 
 dotenv.config();
 const { GITHUB_TOKEN } = process.env;
 
-module.exports = { GITHUB_TOKEN };
+module.exports = {
+  /**
+   * Token from Github API
+   * @type {string}
+   */
+  GITHUB_TOKEN,
+};
